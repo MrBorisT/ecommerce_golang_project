9e66d662cd5aeb94589ea5f51f0b8d9a0ffc8612
Boris <trofimov.boris.92@gmail.com>
2023-02-24 22:52:39 +0900

    added loms handlers to main

--- loms/cmd/app/main.go
+++ loms/cmd/app/main.go
@@ -4,6 +4,10 @@ import (
 	"log"
 	"net/http"
 	"route256/libs/srvwrapper"
+	"route256/loms/internal/handlers/cancelorder"
+	"route256/loms/internal/handlers/createorder"
+	"route256/loms/internal/handlers/listorder"
+	"route256/loms/internal/handlers/orderpayed"
 	"route256/loms/internal/handlers/stockshandler"
 )
 
@@ -11,8 +15,16 @@ const port = ":8081"
 
 func main() {
 
+	createOrder := createorder.New()
+	listOrder := listorder.New()
+	orderPayed := orderpayed.New()
+	cancelOrder := cancelorder.New()
 	stocksHandler := stockshandler.New()
 
+	http.Handle("/createOrder", srvwrapper.New(createOrder.Handle))
+	http.Handle("/listOrder", srvwrapper.New(listOrder.Handle))
+	http.Handle("/orderPayed", srvwrapper.New(orderPayed.Handle))
+	http.Handle("/cancelOrder", srvwrapper.New(cancelOrder.Handle))
 	http.Handle("/stocks", srvwrapper.New(stocksHandler.Handle))
 
 	log.Println("listening http at", port)
