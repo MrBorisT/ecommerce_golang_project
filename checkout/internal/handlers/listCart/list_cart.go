bbc5b84aba14b42fbae890a619df29cf424bc068
Boris <trofimov.boris.92@gmail.com>
2023-02-24 23:00:02 +0900

    list_cart handler fix: wrong bussiness logic function

--- checkout/internal/handlers/listCart/list_cart.go
+++ checkout/internal/handlers/listCart/list_cart.go
@@ -18,25 +18,17 @@ func New(businessLogic *domain.Model) *Handler {
 }
 
 type Request struct {
-	User  int64  `json:"user"`
-	Sku   uint32 `json:"sku"`
-	Count uint16 `json:"count"`
+	User int64 `json:"user"`
 }
 
 var (
-	ErrEmptyUser  = errors.New("empty user")
-	ErrEmptySKU   = errors.New("empty sku")
-	ErrEmptyCount = errors.New("empty count")
+	ErrEmptyUser = errors.New("empty user")
 )
 
 func (r Request) Validate() error {
 	switch {
 	case r.User == 0:
 		return ErrEmptyUser
-	case r.Sku == 0:
-		return ErrEmptySKU
-	case r.Count == 0:
-		return ErrEmptyCount
 	}
 
 	return nil
@@ -51,7 +43,7 @@ func (h *Handler) Handle(ctx context.Context, req Request) (Response, error) {
 
 	var response Response
 
-	err := h.businessLogic.DeleteFromCart(ctx, req.User, req.Sku, req.Count)
+	err := h.businessLogic.ListCart(ctx, req.User)
 	if err != nil {
 		return response, err
 	}
