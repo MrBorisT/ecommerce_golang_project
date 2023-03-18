bb6f7e38925d3e89c1f660183a6c92ed48602951
MrBorisT <trofimov.boris.92@gmail.com>
2023-03-18 16:56:48 +0900

    [GOHW-4]: cancel order by timer

--- loms/internal/domain/create_order.go
+++ loms/internal/domain/create_order.go
@@ -2,8 +2,12 @@ package domain
 
 import (
 	"context"
+	"log"
 	"route256/loms/internal/model"
+	"time"
 )
+
+const DEFAULT_CANCEL_ORDER_TIME = 10 * time.Minute
 
 func (m *service) CreateOrder(ctx context.Context, user int64, items []model.Item) (int64, error) {
 	for _, item := range items {
@@ -17,5 +21,24 @@ func (m *service) CreateOrder(ctx context.Context, user int64, items []model.Ite
 		return 0, err
 	}
 
+	go m.CancelOrderByTimer(ctx, orderID)
+
 	return orderID, nil
+}
+
+func (m *service) CancelOrderByTimer(ctx context.Context, orderID int64) {
+	timer := time.NewTimer(DEFAULT_CANCEL_ORDER_TIME)
+	<-timer.C
+	var status string
+	var err error
+	if status, _, _, err = m.OrderRepository.ListOrder(ctx, orderID); err != nil {
+		log.Printf("getting order when cancelling: %v\n", err)
+	}
+	if status != "new" {
+		return
+	}
+	if err = m.OrderRepository.CancelOrder(ctx, orderID); err != nil {
+		log.Printf("cancel order by timer: %v\n", err)
+	}
+
 }
