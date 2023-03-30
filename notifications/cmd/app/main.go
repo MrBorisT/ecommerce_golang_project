ca8230aa874e3694dd07601a3b6684a1f478debf
MrBorisT <trofimov.boris.92@gmail.com>
2023-03-30 21:54:07 +0900

    [GOHW-6]: notifications subscriber

--- notifications/cmd/app/main.go
+++ notifications/cmd/app/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"route256/libs/kafka"
+	"route256/notifications/config"
+	"route256/notifications/internal/reciever"
+)
+
+func main() {
+	c, err := kafka.NewConsumer(config.ConfigData.Brokers)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	topic := config.ConfigData.Topic
+
+	handlers := map[string]reciever.HandleFunc{
+		topic: func(id string) {
+			log.Println(id)
+		},
+	}
+
+	r := reciever.NewReciever(c, handlers)
+	r.Subscribe(topic)
+
+	<-context.TODO().Done()
 }
